7b1ea6adc99bdc8871088d53c1345c5c325fdd9d
alex_hcj <alexeykorolyov19@gmail.com>
2022-06-06 19:39:38 +0300

    add topics full translation

--- src/app/home-page/topics/topics.component.ts
+++ src/app/home-page/topics/topics.component.ts
@@ -9,11 +9,11 @@ export class TopicsComponent implements OnInit {
   topic = [
     {img: './assets/topics/art-history.png', title: $localize`:Courses category|Art category course:Art History`, quantity: 3},
     {img: './assets/topics/ui-ux.png', title:  $localize`:Courses category|UI & UX category course:UI & UX`, quantity: 16},
-    {img: './assets/topics/3d-design.png', title: '3D Design', quantity: 4},
-    {img: './assets/topics/media-pr.png', title: 'Media & PR', quantity: 9},
-    {img: './assets/topics/programming.png', title: 'Programming', quantity: 2},
-    {img: './assets/topics/music.png', title: 'Music', quantity: 6},
-    {img: './assets/topics/language-courses.png', title: 'Language courses', quantity: 3},
+    {img: './assets/topics/3d-design.png', title: $localize`:Courses category|3D Design category course:3D Design`, quantity: 4},
+    {img: './assets/topics/media-pr.png', title: $localize`:Courses category|Media & PR category course:Media & PR`, quantity: 9},
+    {img: './assets/topics/programming.png', title: $localize`:Courses category|Programming category course:Programming`, quantity: 2},
+    {img: './assets/topics/music.png', title: $localize`:Courses category|Music category course:Music`, quantity: 6},
+    {img: './assets/topics/language-courses.png', title: $localize`:Courses category|Language courses category course:Language courses`, quantity: 3},
   ]
 
   constructor() { }
